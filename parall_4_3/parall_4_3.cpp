8a57b685e0e31ddfe7a1738de1dd9d9f8b34ae9f
cuber201 <daniil.abramov.00@mail.ru>
2024-04-23 18:33:43 +0300

    Добавьте файлы проекта.

---
+++ parall_4_3/parall_4_3.cpp
@@ -0,0 +1,89 @@
+﻿// parall_4_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+void merge(vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1; 
+    int n2 = right - middle;    
+
+    vector<int> L(n1), R(n2); 
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[middle + 1 + j];
+    int i = 0, j = 0;
+    int k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            ++i;
+        }
+        else {
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
+}
+
+void mergeSortParallel(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+#pragma omp parallel sections
+        {
+#pragma omp section
+            mergeSortParallel(arr, left, middle);
+#pragma omp section
+            mergeSortParallel(arr, middle + 1, right);
+        }
+
+        merge(arr, left, middle, right);
+    }
+}
+
+int main() {
+    int n = 1000;
+    vector<int> arr (n);
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+
+    mergeSortParallel(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < arr.size();i++) {
+        cout << arr[i] << " ";
+    }
+    cout <<endl;
+
+    return 0;
+}
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
